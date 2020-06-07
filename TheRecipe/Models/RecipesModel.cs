8560590a5cccf1c8022f03b9491690f08124a90a
Christian Ponce <55345116+christianconcepton@users.noreply.github.com>
2020-06-07 21:18:37 +0800

    Cleaned up RecipesModel

--- TheRecipe/Models/RecipesModel.cs
+++ TheRecipe/Models/RecipesModel.cs
@@ -1,44 +1,13 @@
 namespace TheRecipe
 {
-  using System;
   using System.Data.Entity;
-  using System.ComponentModel.DataAnnotations.Schema;
-  using System.Linq;
-    using System.Data.Entity.Validation;
-    using System.Text;
+  using System.Data.Entity.Validation;
+  using System.Text;
 
-    public partial class RecipesModel : DbContext
+  public partial class RecipesModel : DbContext
   {
-    public RecipesModel()
-        : base("name=TheRecipeModel")
+    public RecipesModel() : base("name=TheRecipeModel")
     {
-    }
-
-    private void SaveChanges(DbContext context)
-    {
-      try
-      {
-        context.SaveChanges();
-      }
-      catch (DbEntityValidationException ex)
-      {
-        StringBuilder sb = new StringBuilder();
-
-        foreach (var failure in ex.EntityValidationErrors)
-        {
-          sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
-          foreach (var error in failure.ValidationErrors)
-          {
-            sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
-            sb.AppendLine();
-          }
-        }
-
-        throw new DbEntityValidationException(
-            "Entity Validation Failed - errors follow:\n" +
-            sb.ToString(), ex
-        ); // Add the original exception as the innerException
-      }
     }
 
     public virtual DbSet<Category> Categories { get; set; }
